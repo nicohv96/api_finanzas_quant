1171641d4fc799fe09135c69030c5b337556df48
Nicolas Villalba <57212697+nicohv96@users.noreply.github.com>
2025-08-27 13:25:57 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,25 +1,19 @@
-from flask import Flask #,jsonify ,request
-# del modulo flask importar la clase Flask y los métodos jsonify,request
-from flask_cors import CORS       # del modulo flask_cors importar CORS
+from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
-app=Flask(__name__)  # crear el objeto app de la clase Flask
-CORS(app) #modulo cors es para que me permita acceder desde el frontend al backend
+app=Flask(__name__)
+CORS(app)
 
-# configuro la base de datos, con el nombre el usuario y la clave
 app.config['SQLALCHEMY_DATABASE_URI']='mysql+pymysql://root:algundia96@localhost/db_finanzas_quant'
-# URI de la BBDD                          driver de la BD  user:clave@URLBBDD/nombreBBDD
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False #none
-
-db= SQLAlchemy(app)   #crea el objeto db de la clase SQLAlquemy
-ma=Marshmallow(app)   #crea el objeto ma de de la clase Marshmallow
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=False
+db= SQLAlchemy(app)
+ma=Marshmallow(app)
 
 from controladores.simbolo_controlador import *
 from controladores.usuario_controlador import *
 
-#from controladores.tipoProducto_controlador import *
-
-# programa principal *******************************
 if __name__=='__main__':  
-    app.run(debug=True, port=5000)    # ejecuta el servidor Flask en el puerto 5000
+    app.run(debug=True, port=5000)
+
