841c45bdfa7238a43822c1e733977894cbc22da3
Nicolas Villalba <57212697+nicohv96@users.noreply.github.com>
2025-08-27 13:25:08 -0300

    Update usuario_modelo.py

--- modelos/usuario_modelo.py
+++ modelos/usuario_modelo.py
@@ -1,22 +1,17 @@
 from sqlalchemy import Column, ForeignKey, Integer, Table
 from sqlalchemy.orm import declarative_base, relationship
 
-from app import app, db          #,ma
+from app import app, db 
 
-# defino las tablas
-class User(db.Model):   # la clase Simbolos hereda de db.Model de SQLAlquemy   
-    id=db.Column(db.Integer, primary_key=True)   #define los campos de la tabla
+class User(db.Model):  
+    id=db.Column(db.Integer, primary_key=True)
     usuario=db.Column(db.String(100))
     passw=db.Column(db.String(100))
     estado=db.Column(db.Boolean, default=False)
-    def __init__(self,usuario,passw,estado=False): #crea el  constructor de la clase
-        # no hace falta el id porque lo crea sola mysql por ser auto_incremento
+    def __init__(self,usuario,passw,estado=False):
         self.usuario=usuario 
         self.passw=passw
         self.estado=estado
 
-#  si hay que crear mas tablas , se hace aqui
-
 with app.app_context():
-    db.create_all()  # aqui crea todas las tablas si es que no estan creadas
-#  ************************************************************
+    db.create_all() 
